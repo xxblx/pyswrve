62f6edfdf5ba2bae2591aa527d3c14729dbaa9b7
Oleg Kozlov <xxblx@posteo.org>
2018-05-11 20:18:37 +0300

    exception template

--- pyswrve/exceptions.py
+++ pyswrve/exceptions.py
@@ -5,4 +5,8 @@ class SwrveApiException(Exception):
     __slots__ = ['error', 'code', 'request_params', 'request_url']
 
     def __init__(self, error, code, request_params, request_url):
+        super().__init__()
+
+    def __str__(self):
+        # TODO
         pass
