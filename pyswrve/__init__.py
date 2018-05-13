780123823446f28ca4f9445808c2a2f920b0523e
Oleg Kozlov <xxblx@posteo.org>
2018-05-13 21:10:58 +0300

    fix init

--- pyswrve/__init__.py
+++ pyswrve/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .api import SwrveExportApi as ExportApi
+from .export_api import SwrveExportApi as ExportApi
+from .userdb_api import SwrveUserdbApi
 
 version_info = (0, 2, 0, 'dev')
 __version__ = '.'.join(map(str, version_info))
