5ecce95b7e56e76951cdadb2b45fb45064f4acdb
Oleg Kozlov <xxblx@posteo.org>
2018-05-17 11:44:37 +0300

    dev version

--- pyswrve/__init__.py
+++ pyswrve/__init__.py
@@ -3,5 +3,5 @@
 from .export_api import SwrveExportApi as ExportApi
 from .userdb_api import SwrveUserdbApi as UserdbApi
 
-version_info = (0, 2, 1)
+version_info = (0, 3, 0, 'dev')
 __version__ = '.'.join(map(str, version_info))
