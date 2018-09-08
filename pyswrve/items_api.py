6f0a8e549bb9c59bf2362453bc3e3883d5b4ed73
Oleg Kozlov <xxblx@posteo.org>
2018-09-09 00:06:02 +0300

    methods for creating/updating items

--- pyswrve/items_api.py
+++ pyswrve/items_api.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import json
 from urllib.parse import urljoin
+
+import requests
 
 from .api import SwrveApi
 
@@ -32,6 +35,21 @@ class SwrveItemsApi(SwrveApi):
         super().__init__(region, api_key, personal_key, section, conf_path)
         self._api_url = urljoin(self._api_url, 'items')
 
+    def send_post_request(self, url, uid=None, data=None):
+        """ Send POST request to Swrve Items API
+
+        :param url: [:class:`str`] url for request
+        :raises SwrveApiException: if request status_code != 200
+        """
+
+        params = self._params.copy()
+        if uid is not None:
+            params['item'] = uid
+        if data is not None:
+            params['data'] = json.dumps(data)
+
+        requests.post(url, data=params)
+
     def get_item_lst(self):
         """ Request list of project items
 
@@ -49,3 +67,29 @@ class SwrveItemsApi(SwrveApi):
 
         results = self.send_api_request(self._api_url, item=uid)
         return results
+
+    def create_item(self, uid, data=None):
+        """ Create or update one item
+
+        :param uid: [:class:`str`] an unique id of the item
+        :param data: [:class:`dict`] a dict of standart parameters and
+            custom attributes of the item, next keys are used as
+            parameters: `name`, `item_class`, `thumbnail`, `description`,
+            and any other keys are used as attributes
+        """
+
+        url = self._api_url
+        self.send_post_request(url, uid, data)
+
+    def create_items(self, data=None):
+        """ Create a batch of items
+
+        :param data: [:class:`dict`] a dict of dicts, where keys are
+            unique items ids and values are dicts of standart parameters
+            and custom attributes of the items (next keys are used as
+            parameters: `name`, `item_class`, `thumbnail`, `description`,
+            and any other keys are used as attributes)
+        """
+
+        url = self._api_url + '_bulk'
+        self.send_post_request(url, data=data)
